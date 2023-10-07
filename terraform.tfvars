7a756cb82797d29744b5f1a241b46785be026cd9
Piyush <https.piyush@gmail.com>
2023-10-07 17:28:54 +0530

    piyush Agrawal

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+cidr_block = "10.0.0.0/16"
+ami = "ami-0f5ee92e2d63afc18"
+instance_type = "t2.micro"
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "cidr_block" {
+  description = "cidr block for myvpc"
+}
+
+variable "ami" {
+  description = "ami for server instance"
+}
+
+variable "instance_type" {
+  description = "instance type"
+}
